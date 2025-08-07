9a86e681b87b69609cdfb608b929c48ad00d1f36
Abr. Sánchez <absanchezmena@gmail.com>
2025-08-07 10:27:56 -0600

    fix conn sql

--- db_connection.py
+++ db_connection.py
@@ -1,14 +1,13 @@
-import pyodbc
+import pymssql
 import pandas as pd
 
 def get_sales_data():
     try:
-        conn = pyodbc.connect(
-            "DRIVER={ODBC Driver 18 for SQL Server};"
-            "SERVER=sql8020.site4now.net;"
-            "DATABASE=db_a91131_test;"
-            "UID=db_a91131_test_admin;"
-            "PWD=dEVOPS2022;"
+        conn = pymssql.connect(
+            server="147.182.194.168",
+            user="sa",
+            password="dEVOPS2022a",
+            database="DENTISALUD"
         )
         query = "SELECT TOP 10 * FROM Prestaciones_Temporal;"
         df = pd.read_sql(query, conn)
